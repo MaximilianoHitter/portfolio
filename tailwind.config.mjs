c585ba30892507be516632a8c96c0aa0ae2ba18e
MaximilianoHitter <mr.vonhumbolt@gmail.com>
2024-09-27 20:13:47 -0300

    cambianding

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
