bb95efd2a00beb8d6c2bef8a61212ba2364f7f7f
MaximilianoHitter <mr.vonhumbolt@gmail.com>
2024-09-28 22:33:20 -0300

    faltan las páginas aparte

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      rotate: {
+        minus20: "-20deg",
+        plus20: "20deg",
+      },
+    },
   },
   plugins: [],
 };
